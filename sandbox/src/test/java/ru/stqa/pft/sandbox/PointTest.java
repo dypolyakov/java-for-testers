073045c183594b565888d525031de7c2a0d90bfb
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-06-02 16:49:11 +0500

    Added several tests for the Point.distance() method - calculating the distance between two points in the plane

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -0,0 +1,36 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.nio.channels.Pipe;
+
+public class PointTest {
+
+    @Test
+    public void distanceTest1() {
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(3, 4);
+
+        double distance = Point.distance(point1, point2);
+        Assert.assertEquals(distance, 5);
+    }
+
+    @Test
+    public void distanceTest2() {
+        Point point1 = new Point(-1, 3);
+        Point point2 = new Point(6, 2);
+
+        double distance = Point.distance(point1, point2);
+        Assert.assertEquals(distance, 7.0710678118654755);
+    }
+
+    @Test
+    public void distanceTest3() {
+        Point point1 = new Point(2, 12);
+        Point point2 = new Point(-1, 8);
+
+        double distance = Point.distance(point1, point2);
+        Assert.assertEquals(distance, 5);
+    }
+}
