e3001ad707962469c5c8864b50da6911eefdf72a
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-06-02 15:04:23 +0500

    Added Point class and .distance() method
    Removed tutorial code from the main method

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,17 +3,13 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        hello("Dmitry");
 
-        Square square = new Square(5);
-        System.out.println("Площадь квадрата со сторонами " + square.length + " = " + square.area());
+        Point point1 = new Point(-1, 3);
+        Point point2 = new Point(6, 2);
 
-        Rectangle rectangle = new Rectangle(6, 7);
-        System.out.println("Площадь прямоугольника со стороной " + rectangle.lengthA + " и " + rectangle.lengthB + " = " + rectangle.area());
-    }
+        System.out.println("Расстояние между двумя точками (" + point1.x + ", " + point1.y + ") и ("
+                + point2.x + ", " + point2.y + ") равно " + Point.distance(point1, point2));
 
-    public static void hello(String somebody) {
-        System.out.println("Hello, " + somebody + "!");
     }
 
 }
