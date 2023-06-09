f336b41944e0ce255c662361cd438b87e3717802
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-06-09 20:08:45 +0500

    Created a test for adding a new contact

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -0,0 +1,71 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ContactCreationTest {
+    private WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        driver.get("http://localhost/addressbook/index.php");
+        driver.findElement(By.name("user")).clear();
+        driver.findElement(By.name("user")).sendKeys(username);
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    @Test
+    public void testContactCreation() {
+        initContactCreation();
+        fillContactForm(new ContactData(
+                "Dmitry",
+                "Polyakov",
+                "DimQA",
+                "+71234567890",
+                "dimqa@dimqa.com"
+        ));
+        submitContactCreation();
+        returnToHomePage();
+    }
+
+    private void returnToHomePage() {
+        driver.findElement(By.linkText("home")).click();
+    }
+
+    private void submitContactCreation() {
+        driver.findElement(By.name("submit")).click();
+    }
+
+    private void fillContactForm(ContactData contactData) {
+        driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+        driver.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+        driver.findElement(By.name("nickname")).sendKeys(contactData.getNickName());
+        driver.findElement(By.name("mobile")).sendKeys(contactData.getPhoneNumber());
+        driver.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    }
+
+    private void initContactCreation() {
+        driver.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.findElement(By.linkText("Logout")).click();
+        driver.quit();
+    }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String lastName;
+    private final String nickName;
+    private final String phoneNumber;
+    private final String email;
+
+    public ContactData(String firstName, String lastName, String nickName, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
