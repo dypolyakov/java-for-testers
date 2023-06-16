33c5c22be23309da5d094bdc14ac5394b9c51b07
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-06-16 14:22:54 +0500

    Optimized type method

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -9,7 +9,7 @@ public class GroupModificationTest extends TestBase {
         app.getNavigationHelper().goToGroupPage();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test3", "test2", "test1"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test1"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
     }
