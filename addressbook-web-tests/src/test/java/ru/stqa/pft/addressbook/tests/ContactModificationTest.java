cb1dc06b00aaa2dcdcee1c4a84cba86c69583fee
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-06-16 18:37:43 +0500

    Added a condition to the contact modification test: if there is no contact, it will be created beforehand

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -8,6 +8,17 @@ public class ContactModificationTest extends TestBase {
     @Test
     public void testContactModification() {
         app.getNavigationHelper().goToHomePage();
+        if (!app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(new ContactData(
+                            "Dmitry",
+                            "Polyakov",
+                            "DimQA",
+                            "+71234567890",
+                            "dimqa@dimqa.com",
+                            "group_test"
+                    ),
+                    true);
+        }
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData(
                 "Vasya",
