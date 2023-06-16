eb5586b933ee1a3ea77b9e5ddbf3902dd9f204dc
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-06-16 12:59:29 +0500

    Added Browser interface and browser launch conditions

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,20 +1,35 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+    private final Browser browser;
     WebDriver driver;
     private GroupHelper groupHelper;
     private NavigationHelper navigationHelper;
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
 
+    public ApplicationManager(Browser browser) {
+        this.browser = browser;
+    }
+
     public void init() {
-        driver = new FirefoxDriver();
+        if (browser.equals(Browser.FIREFOX)) {
+            driver = new FirefoxDriver();
+        } else if (browser.equals(Browser.CHROME)) {
+            driver = new ChromeDriver();
+        } else if (browser.equals(Browser.SAFARI)) {
+            driver = new SafariDriver();
+        }
+
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         groupHelper = new GroupHelper(driver);
         navigationHelper = new NavigationHelper(driver);
