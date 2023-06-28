25f1ca45abe344e42f676e3995c939993f7887e7
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-06-28 19:32:59 +0500

    Group creation test has been improved. Added group list comparison

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTest extends TestBase {
@@ -11,13 +12,21 @@ public class GroupCreationTest extends TestBase {
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().goToGroupPage();
+
         List<GroupData> before = app.getGroupHelper().getGroupList();
+
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("group_test", null, null));
+        GroupData group = new GroupData("group_test", null, null);
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
+
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(before.size() + 1, after.size());
+
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        before.add(group);
+        Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
     }
 
 }
