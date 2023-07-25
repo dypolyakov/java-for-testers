2e76811156439b8d0ead1616bb3c9a7e6af8ec9e
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-07-25 19:27:47 +0500

    Optimized test of group deletion using the list of groups from the database

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -12,19 +12,20 @@ public class GroupDeletionTest extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
-        app.goTo().groupPage();
-        if (app.group().all().size() == 0) {
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
             app.group().create(new GroupData().withName("group for deletion").withHeader("header").withFooter("footer"));
         }
     }
 
     @Test
     public void testGroupDeletion() {
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         GroupData deletedGroup = before.iterator().next();
+        app.goTo().groupPage();
         app.group().delete(deletedGroup);
         assertThat(before.size() - 1, equalTo(app.group().count()));
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(before.without(deletedGroup), equalTo(after));
     }
 
